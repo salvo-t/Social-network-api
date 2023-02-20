ffd426ff078b88ede7f6dd1086e06be3e6f5c14c
salvo-t <salvot.biz@gmail.com>
2023-02-19 23:25:41 -0500

    finished thought models

---
+++ models/Thought.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction')
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            max_length: 280,
+            min_length: 1
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (data) => new Date(data)
+        },
+
+        username: {
+            type: String,
+            required: true
+        },
+
+    reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+)
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reaction.length
+});
+
+const Thought = model('Thought', thoughtSchema)
+
+module.exports = Thought;
